33c00fbcfa3e83a951bdef5f98b696dfb91b35a7
Serena Lertkantitham <lertkantitham@gmail.com>
2019-02-16 09:11:58 -0800

    add runServer and closeServer func

--- server.js
+++ server.js
@@ -1,12 +1,51 @@
+'use strict';
+
 const express = require('express');
 const morgan = require('morgan');
+const mongoose = require('mongoose');
 
 const app = express();
+const { DATABASE_URL, PORT } = require('./config');
 
 app.use(express.static('public'));
 app.use(morgan('common'));
 app.use(express.json());
 
-app.listen(process.env.PORT || 8080);
+let server;
+function runServer(databaseUrl, port = PORT ) { 
+  return new Promise((resolve, reject) => {
+    mongoose.connect(databaseUrl, err => {
+      if (err) {
+        return reject(err);
+      }
+      server = app.listen(port, () => {
+        console.log(`Your app is now listening on port ${port}`);
+        resolve();
+      })
+      .on('error', err => {
+        mongoose.disconnect();
+        reject(err);
+      });
+    });
+  });
+}
 
-module.exports = { app };
+function closeServer() {
+  mongoose.disconnect().then(() => {
+    return new Promise((resolve, reject) => {
+      console.log(`Closing server`);
+      server.close(err => {
+        if (err) {
+          return reject(err);
+        }
+        resolve();
+      });
+    });
+  });
+}
+
+if (require.main === module) {
+  runServer(DATABASE_URL).catch(err => console.log(err));
+}
+
+module.exports = { app, runServer, closeServer };
--- test/test-server.js
+++ test/test-server.js
@@ -4,12 +4,14 @@ const chai = require('chai');
 const chaiHttp = require('chai-http');
 
 const { app, runServer, closeServer } = require('../server');
+const { TEST_DATABASE_URL } = require('../config');
 
 const expect = chai.expect;
 
 chai.use(chaiHttp);
 
 describe('Application', function() {
+
   it('should display home page when opened', function() {
     return chai.request(app)
       .get('/')
